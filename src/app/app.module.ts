2fd93d36757eb0024a0f4a0d8244a33064543939
jean.santini <jean.santini@db1.com.br>
2022-09-09 10:55:01 -0300

    Estratégia de navegação hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { PreloadAllModules, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { routes } from './app-routing.module';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { PreloadAllModules, RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { HomeComponent } from './home/home.component'
+import { routes } from './app-routing.module'
+import { RestaurantsComponent } from './restaurants/restaurants.component'
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
+import { MenuComponent } from './restaurant-detail/menu/menu.component'
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component'
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { OrderSummaryComponent } from './order-summary/order-summary.component'
+import { SharedModule } from './shared/shared.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NotFoundComponent } from './not-found/not-found.component'
+import { LocationStrategy, HashLocationStrategy } from '@angular/common'
 
 
 @NgModule({
@@ -43,6 +44,7 @@ import { NotFoundComponent } from './not-found/not-found.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
   ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
