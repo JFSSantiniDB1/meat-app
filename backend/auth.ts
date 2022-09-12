b2ce7bece83731b0a891a83404f3a083216355a0
jean.santini <jean.santini@db1.com.br>
2022-09-12 14:56:01 -0300

    atualização de segurança, token

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,15 +1,18 @@
-import {Request, Response} from 'express'
-import {User, users} from './users'
+import { Request, Response } from 'express'
+import { User, users } from './users'
+import * as jwt from 'jsonwebtoken'
 
 export const handleAuthentication = (req: Request, resp: Response) => {
     const user = req.body
     if(isValid(user)){
-        const dbUser: User = users[user.email]
-        resp.json({name: dbUser.name, email: dbUser.email})
+        const dbUser = users[user.email]
+        const token = jwt.sign({sub: dbUser.email, iss: 'meap-app'}, 'meat-app-password')
+        resp.json({name: dbUser.name, email: dbUser.email, accessToken: token})
     }else{
         resp.status(403).json({message: 'Dados inválidos.'})
     }
 }
+
 
 function isValid(user: User): boolean {
     if(!user){
---
+++ backend/dist/authz.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.handleAuthorization = void 0;
+var handleAuthorization = function (req, resp, next) {
+};
+exports.handleAuthorization = handleAuthorization;
